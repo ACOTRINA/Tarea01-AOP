c11688994a893f2ef23b73d08588732329f1267e
sgloayza <sgloayza@espol.edu.ec>
2020-11-07 16:44:31 -0500

    funcionalidad extra

--- src/application/Logger.aj
+++ src/application/Logger.aj
@@ -1,11 +1,26 @@
 package application;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public aspect Logger {
 	
-	pointcut successA() : call(* cambiarColor*(..) );
-    after() : successA() { 
+	pointcut success() : call(* cambiarColor*(..) );
+    after() : success() { 
     	System.out.println("**** Color cambiado ****");    	
     }
-
     
+    pointcut successA() : call(* abrirArchivo*(..) );    
+    after() : successA() {
+    	System.out.println("Someone has performed open operation with the following file: Color.txt");
+    }
+    
+    pointcut successC() : call(* cerrarArchivo*(..) );    
+    after() : successC() {
+    	System.out.println("Someone has performed close operation with the following file: Color.txt");
+    }
 }
+
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,12 @@
 package application;
 	
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -14,32 +21,38 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 
+
+
 public class Main extends Application {
     StackPane root1;
-    VBox root2; 
-    Rectangle rect;
+    VBox root2;   
     Button btn1;
     Button btn2;
     Button btn3;
-
+    Rectangle rect;
+    
     @Override
     public void start(Stage primaryStage) {
-    	btn1 = new Button();
+        btn1 = new Button();
         btn2 = new Button();
         btn3 = new Button();
         btn1.setText("Amarillo");
         btn2.setText("Azul");
         btn3.setText("Rojo");
-
         btn1.setStyle("-fx-background-color: pink;-fx-font-size: 1.5em;-fx-font-weight:bolder; -fx-font: bold italic 15pt \"Lucida Console\"");
         btn2.setStyle("-fx-background-color: pink;-fx-font-size: 1.5em;-fx-font-weight:bolder; -fx-font: bold italic 15pt \"Lucida Console\"");
         btn3.setStyle("-fx-background-color: pink;-fx-font-size: 1.5em;-fx-font-weight:bolder; -fx-font: bold italic 15pt \"Lucida Console\"");
-
+        
         btn1.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 String color = btn1.getText();
             	cambiarColor(color);
+            	try {
+					guardarCambioArchivo(color);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
             }			
         });
         
@@ -48,6 +61,11 @@ public class Main extends Application {
             public void handle(ActionEvent event) {
             	String color = btn2.getText();
             	cambiarColor(color);
+            	try {
+					guardarCambioArchivo(color);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
             }
         });
         
@@ -56,9 +74,14 @@ public class Main extends Application {
             public void handle(ActionEvent event) {
             	String color = btn3.getText();
             	cambiarColor(color);
+            	try {
+					guardarCambioArchivo(color);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
             }
         });
-
+        
         rect = new Rectangle(300,250,300,250);
         rect.setFill(Color.TRANSPARENT);
         
@@ -75,7 +98,8 @@ public class Main extends Application {
         
         primaryStage.setTitle("TAREA01-AOP");
         primaryStage.setScene(scene);
-        primaryStage.show();        
+        primaryStage.show();
+        
     }
 
     public static void main(String[] args) {
@@ -87,4 +111,24 @@ public class Main extends Application {
         if(color=="Amarillo") rect.setFill(Color.YELLOW);
         else if(color=="Azul") rect.setFill(Color.BLUE);
         else rect.setFill(Color.RED);
-    }}
+    }
+    
+    private void guardarCambioArchivo(String color) throws IOException {
+        BufferedWriter bw =abrirArchivo();
+        SimpleDateFormat dateFormat  = new SimpleDateFormat("HH:mm:ss");
+        Date date = new Date();
+        bw.append("Color: "+color+"- hora: "+dateFormat.format(date));
+        bw.newLine();
+        cerrarArchivo(bw);        
+    }
+
+	private BufferedWriter abrirArchivo() throws IOException {		
+		return new BufferedWriter(new FileWriter(new File("Color.txt"),true));
+	}
+	private void cerrarArchivo(BufferedWriter bw) throws IOException {		
+		bw.close();
+	}
+}
+
+
+
