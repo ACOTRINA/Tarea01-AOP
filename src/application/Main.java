e0bdf6d7c4d1f4f2569cf38d963989f89341c94b
NataliaMw <mnmawyin@espol.edu.ec>
2020-11-07 14:45:02 -0500

    Botones

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,15 +1,25 @@
 package application;
 	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+    Button btn1;
+    Button btn2;
+    Button btn3;
 	@Override
 	public void start(Stage primaryStage) {
-
+    	btn1 = new Button();
+        btn2 = new Button();
+        btn3 = new Button();
+        btn1.setText("Amarillo");
+        btn2.setText("Azul");
+        btn3.setText("Rojo");
 		System.out.println("d");
 	}
 	
@@ -17,3 +27,4 @@ public class Main extends Application {
 		launch(args);
 	}
 }
+
